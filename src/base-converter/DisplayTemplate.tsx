4f5c4ecf93662ee7763732b55540e139eb0656cc
atobaum <atobaum@gmail.com>
2020-11-01 01:05:23 +0900

    style: Change display template style

--- src/base-converter/DisplayTemplate.tsx
+++ src/base-converter/DisplayTemplate.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import styled from "styled-components";
 
 const DisplayTemplateBlock = styled.div<{ focused: boolean }>`
-  height: 4rem;
+  .name-display {
+    padding: 0.2rem 0.5rem;
+    color: gray;
+    text-align: left;
+  }
 
   .value-display {
     font-size: 2.5rem;
+    text-align: right;
+    padding: 0 0.5rem;
   }
   border: ${(props) => {
     return props.focused ? "2px solid yellow;" : "2px solid black;";
@@ -18,7 +24,7 @@ const DisplayTemplate: React.FC<{
 }> = ({ name, focused, onClick, children }) => {
   return (
     <DisplayTemplateBlock onClick={onClick} focused={focused}>
-      <div>{name}</div>
+      <div className="name-display">{name}</div>
       <div className="value-display">{children}</div>
     </DisplayTemplateBlock>
   );
