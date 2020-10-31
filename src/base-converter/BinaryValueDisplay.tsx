ddb687fa1175804ca5dae3b4e943a154c1fcdeaf
atobaum <atobaum@gmail.com>
2020-11-01 01:06:15 +0900

    feat: Toggle digits in binary display by click #1

--- src/base-converter/BinaryValueDisplay.tsx
+++ src/base-converter/BinaryValueDisplay.tsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { PointerEvent } from "react";
 
-const BinaryValueDisplay: React.FC<{ value: number }> = ({ value }) => {
-  const toString = (v: number) => {
-    if (value === 0) return "0";
-    let result = "";
+const BinaryValueDisplay: React.FC<{
+  value: number;
+  onChange: (value: number) => void;
+}> = ({ value, onChange }) => {
+  const toDigits = (v: number): number[] => {
+    if (v <= 0) return [0];
+
+    const result = [];
     while (v) {
-      result = (v % 2) + result;
+      result.push(v % 2);
       v = ~~(v / 2);
     }
-    return result;
+    return result.reverse();
   };
 
-  return <div>{toString(value)}</div>;
+  const onClickDigit = (e: PointerEvent<HTMLSpanElement>): void => {
+    const index = parseInt((e.target as HTMLSpanElement).dataset.idx!);
+
+    if (isNaN(index)) throw new Error("error");
+
+    if ((value >> index) & 1) onChange(value & ~(1 << index));
+    else onChange(value | (1 << index));
+  };
+
+  const digits = toDigits(value);
+
+  return (
+    <div>
+      {digits.map((v, idx) => (
+        <span
+          key={idx}
+          data-idx={digits.length - idx - 1}
+          onPointerDown={onClickDigit}
+        >
+          {v}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default BinaryValueDisplay;
